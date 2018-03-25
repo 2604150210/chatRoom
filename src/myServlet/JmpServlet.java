10aac47f84460d173f9088976ab71a3c94fab75c
zj <zj@webturing.com>
2018-03-25 16:57:20 +0800

    init

---
+++ src/myServlet/JmpServlet.java
@@ -0,0 +1,13 @@
+package myServlet;
+
+import java.io.IOException;
+
+public class JmpServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request,response);
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ZHAO Jing
+  Date: 2018/3/24
+  Time: 16:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Log In</title>
+  </head>
+  <body>
+  <form action="jmpServlet">
+     please input your nickname:
+    <input type="text"><br>
+    <input type="submit" value="submit">
+
+    <div class="container">
+
+    </div>
+  </form>
+  </body>
+</html>
